f3a60df707b4fd99225ea6dc7b9e27661b559a2f
fernoult <francois.buza@gmail.com>
2015-06-09 16:50:09 +0200

    yeah

--- src/jwst/views/JWSTFrame.java
+++ src/jwst/views/JWSTFrame.java
@@ -2,13 +2,19 @@ package jwst.views;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class JWSTFrame extends JFrame {
 
+	private JButton _btnExit;
 	public JWSTFrame() throws HeadlessException {
 		initJWSTFrame();
 	}
@@ -31,6 +37,20 @@ public class JWSTFrame extends JFrame {
 	private void initJWSTFrame(){
 		
 		setLayout(new BorderLayout());
+		
+		_btnExit = new JButton("Exit");
+		_btnExit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		JPanel pane = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		pane.add(_btnExit);
+		
+		add(pane, BorderLayout.SOUTH);
 		
 		setSize(new Dimension(400, 200));
 		setLocationRelativeTo(null);
